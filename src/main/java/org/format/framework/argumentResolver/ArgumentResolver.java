28927d73fd7f2f43db2b0f90dbf9c584425eb44b
format <fangjian0423@gmail.com>
2014-07-08 00:30:17 +0800

    add ArgumentResolver interface and DataBinder interface

---
+++ src/main/java/org/format/framework/argumentResolver/ArgumentResolver.java
@@ -0,0 +1,11 @@
+package org.format.framework.argumentResolver;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fangjian
+ * Date: 14-7-7
+ * Time: 下午11:58
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ArgumentResolver {
+}
---
+++ src/main/java/org/format/framework/bind/DataBinder.java
@@ -0,0 +1,11 @@
+package org.format.framework.bind;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fangjian
+ * Date: 14-7-8
+ * Time: 上午12:26
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DataBinder {
+}
